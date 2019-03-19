ed5ad8d6a19089bdcbd67de225510199cc41e210
Ian Bissett <ian.bissett@bigcommerce.com>
2019-03-20 10:08:10 +1100

    Initial Commit

---
+++ ninja-level-12/dog/dog.go
@@ -0,0 +1,7 @@
+// package dog implemetns math functions for dogs
+package dog
+
+// Years converts human years to dog years and returns an int
+func Years(y int) int {
+    return y * 7
+}
---
+++ ninja-level-12/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "github.com/GoesToEleven/go-programming/code_samples/bisscuitt/dog"
+    "fmt"
+)
+
+func main() {
+    dy := dog.Years(5)
+    fmt.Printf("That's %d dog years\n", dy)
+}
