a7365aa0a123cb8546d8615797e9eb7838a91add
Ian Bissett <ian.bissett@bigcommerce.com>
2019-03-20 11:14:41 +1100

    Adding benchmarks and Exmaples

--- ninja-level-12/dog/dog_test.go
+++ ninja-level-12/dog/dog_test.go
@@ -2,10 +2,24 @@
 package dog
 
 import (
+    "testing"
 	"fmt"
 )
+
+func TestYears(t *testing.T) {
+    v := Years(1)
+    if v != 7 {
+        t.Error("Expected: 7, Got:", v)
+    }
+}
 
 func ExampleYears() {
 	fmt.Println(Years(10))
 	// Output: 70
+}
+
+func BenchmarkYears(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Years(7)
+    }
 }
